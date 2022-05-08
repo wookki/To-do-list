2755d9fdcc20ae9325430f2a6b3a478cdd7604cf
wookki <bwlimtony@gmail.com>
2022-05-08 22:05:47 +0900

    My-To-Do-list ver_1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
-let text = document.querySelector('.input input').value;
-const list_x = document.querySelector('.list1 .x-btn')
-console.log(text);
+const input = document.querySelector("#input input");
+const button = document.querySelector("#input button");
+
+button.addEventListener('click', function() {
+  const inputstring = input.value;
+  if(inputstring === '') {
+    alert('empty')
+    return;
+  } 
+  addItem(inputstring);
+
+})
+
+function addItem(str) {
+  const list = document.querySelector('.List');
+
+  const li = document.createElement('li');
+  li.classList.add('item-li');
+
+  const itemName = document.createElement('span');
+  itemName.innerHTML = str;
+  itemName.classList.add('item-title');
+
+  const cancleBtn = document.createElement('div');
+  cancleBtn.classList.add('material-icons')
+  cancleBtn.innerHTML = 'close';
+  cancleBtn.addEventListener('click', function () {
+    onHandleRemoveButton(li);
+  })  
+  
+  
+  li.append(itemName);
+  li.append(cancleBtn);
+
+  list.append(li);
+}
+
+
+
+function onHandleRemoveButton(li) {
+  li.remove();
+}
